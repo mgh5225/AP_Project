ffc92613f19bbd77fafeb660fb774693864ab0f7
arianpotter <45937094+arianpotter@users.noreply.github.com>
2019-07-08 13:18:10 +0430

    file parameters updated

--- AP_Project/file.h
+++ AP_Project/file.h
@@ -8,6 +8,7 @@ protected:
     QString manager_ID;
     have_file* building;
     bool isAccepted;
+    QString user_ID;
 public:
     file();
     virtual~file();
@@ -15,6 +16,10 @@ public:
     virtual double final_price()=0;
     QString get_building_ID();
     QString get_manager_ID();
+    QString get_user_ID();
+    void set_user_ID(QString);
+    bool get_is_accpted();
+    void set_is_accpted(bool);
     void set_building(have_file*);
 
 };
